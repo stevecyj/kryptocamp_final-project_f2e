8838aa51894083543e3603c50caeb2a9805048d7
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-04-24 11:23:30 +0800

    feat: add error page component for handling route errors
    - Add an error page component to handle route errors in the router
    - Create an `ErrorPage` component to display error messages to the user
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './routes/root';
+import ErrorPage from './pages/error-page.jsx';
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
