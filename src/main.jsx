28f2ae16dfb86faccefa385272aecf98a3f2c2d6
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-04-24 11:09:55 +0800

    feat: refactor main.jsx and add new root route file
    - Refactor the main.jsx file
    - Add a new file, src/routes/root.jsx
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {createBrowserRouter,RouterProvider} from "react-router-dom";
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+import './index.css';
 
-const router = createBrowserRouter([{
-    path: "/",
-    element:<div>Hello world</div>
-}]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
